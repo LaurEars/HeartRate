c055637b4ca6297dc82a13e5d783984241befdda
Laura Rupprecht <laura.rupprecht@server.fake>
2014-03-11 20:53:12 -0400

    Added time attribute to Walk

--- walk.py
+++ walk.py
@@ -3,6 +3,11 @@ __author__ = 'Laura'
 
 import csv
 import numpy as np
+from datetime import datetime
+
+
+def convert_to_datetime(sometime):
+    return datetime.strptime(sometime, '%Y-%m-%dT%H:%M:%S.%fZ')
 
 
 class Walk(object):
@@ -14,26 +19,29 @@ class Walk(object):
         activity: physical activity associated with file (e.g. walking, biking, running)
         description: any description provided by the user in at the time file was written
         hr: heart rate data, as a numpy float array
+        time: datetime object containing an array of recorded point times
     """
-    # TODO: Implement conversion from data points to time points
     def __init__(self, file_name):
         self.file_name = file_name
         with open(file_name, 'rb') as f:
             reader = csv.reader(f)
-            reader.next() # skips first line
+            reader.next()  # skips first line
             line = reader.next()
             self.name = line[0]
             self.activity = line[1]
             self.description = line[2]
-            reader.next() # skips blank line
+            reader.next()  # skips blank line
             headers = reader.next()
             hr_index = headers.index('Heart rate (bpm)')
+            time_index = headers.index('Time')
             self.hr = []
+            self.time = []
             for line in reader:
                 try:
                     # might change error-checking in heart rate values here
                     if not (line[hr_index] == '' or line[hr_index] == '-1.0'):
                         self.hr.append(line[hr_index])
+                    self.time.append(convert_to_datetime(line[time_index]))
                 except IndexError:
                     break
             self.hr = np.array(self.hr, dtype=float)
